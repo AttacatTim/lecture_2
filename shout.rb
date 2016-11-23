ddccb1e417310c4bfc26997ec025ff57feecca69
AttacatTim <tim.barlow@attacat.co.uk>
2016-11-23 20:42:27 +0000

    first ruby programs

---
+++ shout.rb
@@ -0,0 +1,9 @@
+postcode_text = "postcode please luv"
+email_text = "whats yer email?"
+
+puts postcode_text
+user_input = gets.chomp
+puts user_input.upcase
+puts email_text
+user_email = gets.chomp
+puts user_email + "? Really, that's the same as mine! Now i can spam you to death"
